932adf47e02acedb40dd90050797feca7b90ec79
1ashutoshverma <ashu9236047613@gmail.com>
2023-11-22 20:47:17 +0530

    Port addd

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   res.send("Api is working");
 });
 
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("listenig at port 8080");
 });
